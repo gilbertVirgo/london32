88375619770d537720397883350e32cdc0f92f14
gilbertVirgo <gilbertjvirgo@gmail.com>
2026-06-24 13:20:31 +0100

    Changed the interval on HomeNonDenom glyphs

--- src/sections/HomeNonDenom.tsx
+++ src/sections/HomeNonDenom.tsx
@@ -10,7 +10,7 @@ const HomeNonDenom = () => {
 	useEffect(() => {
 		const interval = setInterval(() => {
 			setActiveIndex((prev) => (prev + 1) % 3);
-		}, 1250);
+		}, 2000);
 		return () => clearInterval(interval);
 	}, []);
 
